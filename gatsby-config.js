d54512eed5d2177627e50506e0273d5787933cfa
Martin Fekete <martin.fekete@student.tuke.sk>
2021-10-04 22:15:36 +0200

    add main.yml and google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require(`dotenv`).config({
+  path: `.env`,
+})
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
@@ -15,6 +19,17 @@ module.exports = {
         path: "./src/images/",
       },
       __key: "images",
+    },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
     },
   ],
 };
